08354efb416cdcd34def4ed6b079d74a11cd5246
Dee Torna <tornadavinder@gmail.com>
2023-09-16 15:21:35 +0000

    basic game is created now to add extra feature such as timer and sounds

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,96 +1,108 @@
 const questionElement = document.getElementById("question");
 const optionsElement = document.getElementById("options");
-const nextButton = document.getElementById("start");
+const nextButton = document.getElementById("next");
 const scoreElement = document.getElementById("score");
 const difficultyButtons = document.querySelectorAll(".difficulty button");
+const startGameButton = document.getElementById("start-game");
 
 let currentDifficulty = "easy";
 let currentQuestionIndex = 0;
 let score = 0;
 let questions = {};
 
+let gameStarted = false;
+
 // Function to fetch and load questions from the JSON file
 function loadQuestions() {
-  fetch("../questions.json")
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-      questions = data;
-      randomizeQuestions();
-      setQuestion(); // Call setQuestion after loading and randomizing questions
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+    fetch("../questions.json")
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data);
+            questions = data;
+            randomizeQuestions();
+            setQuestion();
+        })
+        .catch((error) => {
+            console.error(error);
+        });
 }
 
 // Function to randomize questions within each difficulty level
 function randomizeQuestions() {
-  for (const difficulty in questions) {
-    questions[difficulty] = shuffleArray(questions[difficulty]);
-  }
+    for (const difficulty in questions) {
+        questions[difficulty] = shuffleArray(questions[difficulty]);
+    }
 }
 
 // Function to shuffle an array (Fisher-Yates shuffle)
 function shuffleArray(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-  return array;
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
 }
 
-// Function To Set Current Question //
+// Function To Set Current Question
 function setQuestion() {
-  const currentQuestion = questions[currentDifficulty][currentQuestionIndex];
-  if (!currentQuestion) {
-    endQuiz();
-    return;
-  }
-  questionElement.textContent = currentQuestion.question;
-  optionsElement.innerHTML = "";
-  currentQuestion.options.forEach((option) => {
-    const button = document.createElement("button");
-    button.textContent = option;
-    button.addEventListener("click", () => checkAnswer(option));
-    optionsElement.appendChild(button);
-  });
+    const currentQuestion = questions[currentDifficulty][currentQuestionIndex];
+    if (!currentQuestion) {
+        endQuiz();
+        return;
+    }
+    questionElement.textContent = currentQuestion.question;
+    optionsElement.innerHTML = "";
+    currentQuestion.options.forEach((option) => {
+        const button = document.createElement("button");
+        button.textContent = option;
+        button.addEventListener("click", () => checkAnswer(option));
+        optionsElement.appendChild(button);
+    });
 }
 
-// Function To Check The Correct Answer //
+// Function To Check The Correct Answer
 function checkAnswer(selectedOption) {
-  const currentQuestion = questions[currentDifficulty][currentQuestionIndex];
-  if (selectedOption === currentQuestion.correctOption) {
-    score++; // Increment the score for correct answers
-  }
-  currentQuestionIndex++;
-  scoreElement.textContent = `Score: ${score}`; // Update the displayed score
-  setQuestion();
+    const currentQuestion = questions[currentDifficulty][currentQuestionIndex];
+    if (selectedOption === currentQuestion.correctOption) {
+        score++;
+    }
+    currentQuestionIndex++;
+    scoreElement.textContent = `Score: ${score}`;
+    setQuestion();
 }
 
-// Function To End Quiz //
+// Function To End Quiz
 function endQuiz() {
-  questionElement.textContent = "Quiz completed!";
-  optionsElement.innerHTML = "";
-  nextButton.style.display = "none";
-  scoreElement.textContent = `Final Score: ${score}`;
+    questionElement.textContent = "Quiz completed!";
+    optionsElement.innerHTML = "";
+    nextButton.style.display = "none";
+    scoreElement.textContent = `Final Score: ${score}`;
 }
 
 nextButton.addEventListener("click", () => {
-  currentQuestionIndex++;
-  setQuestion();
+    currentQuestionIndex++;
+    setQuestion();
 });
 
 difficultyButtons.forEach((button) => {
-  button.addEventListener("click", () => {
-    currentDifficulty = button.id;
-    currentQuestionIndex = 0;
-    score = 0;
-    loadQuestions(); // Load questions when difficulty level changes
-    nextButton.style.display = "block";
-    scoreElement.textContent = `Score: ${score}`;
-  });
+    button.addEventListener("click", () => {
+        currentDifficulty = button.id;
+        currentQuestionIndex = 0;
+        score = 0;
+        loadQuestions();
+        nextButton.style.display = "block";
+        scoreElement.textContent = `Score: ${score}`;
+        startGameButton.style.display = "none"; // Hide the Start Game button when difficulty changes
+    });
 });
 
-// Start by loading questions when the page loads
-loadQuestions();
+startGameButton.addEventListener("click", startGame);
+
+// Function to start the game
+function startGame() {
+    if (!gameStarted) {
+        gameStarted = true;
+        loadQuestions();
+        startGameButton.style.display = "none";
+    }
+}
